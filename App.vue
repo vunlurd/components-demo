28ecc3c7426bcfb590d1705d6a8585584f40d546
Vunlurd <vunlurd@yahoo.com>
2021-11-19 16:21:15 +0100

    Working example but with bugs in styles

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h2>{{ header }}</h2>
+    <p>{{ content }}</p>
+    <hr />
+    <p>Edit header: <input v-model="header" placeholder="Type new header here..." /></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      header: 'Turbo Project Demo',
+      content: 'Resize the browser window to see how your content is responsive to the size of the window.'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Roboto', sans-serif;
+  padding-left: 16px;
+}
+</style>
